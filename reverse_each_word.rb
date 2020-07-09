5fb5f2eced746f755c3e1566f85439635d025cf7
adept-bell-4492 <garlandegoma@gmail.com>
2020-07-09 20:09:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,10 @@
 def reverse_each_word(string)
+  array=string.split(" ") #turn string into an array
+  new_array=[]
+  array.each do |string|
+    return_array<< string.reverse 
+  end
+  new_array.join(" ")
+end
+    
+    
